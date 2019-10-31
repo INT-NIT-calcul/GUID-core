770157ff01f8db962da05c0f91572b1ed344e774
Slowblitz <jeremygarcia937@gmail.com>
2019-10-31 09:58:40 +0100

    Add files via upload

---
+++ scripts/GUID_CORE/generate_GUID_Bash.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+from generate_GUID import generate_GUID
+
+import hashlib
+
+
+if len(sys.argv) > 1:
+
+    if len(sys.argv) == 5:
+        key = sys.argv[1]+sys.argv[2]+sys.argv[3]+sys.argv[4]
+        print(generate_GUID(key))
+
+    if len(sys.argv) == 2:
+        key = sys.argv[1]
+        print(generate_GUID(key))
+
+    else:
+        print("mauvais nombre d'arguments")
