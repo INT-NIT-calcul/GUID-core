2922534dfd0d204a733254aa993a17e761dbc9e5
Slowblitz <jeremygarcia937@gmail.com>
2019-11-04 13:37:16 +0100

    Add files via upload

--- scripts/GUID_CORE/generate_GUID.py
+++ scripts/GUID_CORE/generate_GUID.py
@@ -13,15 +13,3 @@ def generate_GUID(keyInput):
     return GUID
 
 
-if len(sys.argv) > 1:
-
-    if len(sys.argv) == 5:
-        key = sys.argv[1]+sys.argv[2]+sys.argv[3]+sys.argv[4]
-        print(generate_GUID(key))
-
-    if len(sys.argv) == 2:
-        key = sys.argv[1]
-        print(generate_GUID(key))
-
-    else:
-        print("mauvais nombre d'arguments")
